4f725d6ddec3ae1847b4cfe5ead7e8bd9d62da93
BhagyaSri-1806 <bhagyasrilakshmi18@gmail.com>
2024-11-28 16:43:33 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import random
-
-# Generate and print 100 random numbers
-for i in range(100):
-    # Generate random number between 1 and 1000
-    random_number = random.randint(1, 1000)
-    print(f"Number {i+1}: {random_number}")
